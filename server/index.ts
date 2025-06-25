c6be7aa251a1fcf7e1622e22127df3579c7e4fb1
yalayo <60822416+yalayo@users.noreply.github.com>
2025-06-25 06:34:29 +0000

    [yun] Rolling back this changes.

--- server/index.ts
+++ server/index.ts
@@ -60,11 +60,14 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "127.0.0.1", 
-    // reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  server.listen(
+    {
+      port,
+      host: "0.0.0.0",
+      reusePort: true,
+    },
+    () => {
+      log(`serving on port ${port}`);
+    },
+  );
 })();
